9be438b63be4bb75ac5906c1fe2293d58040ef75
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-06-21 10:44:08 +0100

    add component to add smurfs

---
+++ smurfs/src/components/Container.js
@@ -0,0 +1,14 @@
+import  React from 'react';
+import Smurfs from './Smurfs';
+import SmurfForm from './SmurfForm';
+
+const Container = () => {
+    return(
+        <div>
+            <SmurfForm />
+            <Smurfs />
+        </div>
+    )
+}
+
+export default Container;
